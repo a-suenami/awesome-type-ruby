f730f90be83f630e7efb1f4b0f351cb4d02d53be
Akira SUENAMI <a.suenami@gmail.com>
2019-03-14 15:39:40 +0900

    Reinitialize gem.

---
+++ lib/awesome_type.rb
@@ -0,0 +1,6 @@
+require "awesome_type/version"
+
+module AwesomeType
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/awesome_type/version.rb
@@ -0,0 +1,3 @@
+module AwesomeType
+  VERSION = "0.1.0"
+end
